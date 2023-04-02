948a84268c21ba4f129363085eb08fd596b98165
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-04-02 23:31:48 +0600

    practise code

---
+++ C-programming/Module-2/Arithmatic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a=10,b=20,c=3;
+int sum=a+b;
+printf("%d\n",sum);
+float div=b*1.0/c;
+printf("%f\n",div);
+int mod=20%3;
+printf("%d",mod);
+
+return 0;
+}
---
+++ C-programming/Module-2/conditional.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	
+	
+	int tk;
+	scanf("%d",&tk);
+	if(tk>=100){
+		printf("Buger Khabo");
+		
+	}
+	else
+	{
+		printf("burger Khabo na");
+	}
+}
