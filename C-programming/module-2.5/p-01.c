ea9551b39fa59b3e49106af9270d75effa394217
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-04-06 12:37:22 +0600

    practise

---
+++ C-programming/module-2.5/p-01.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char c='%';
+
+
+    printf("Recently I heard that you’ve achieved 95%c marks in your exam.This is brilliant!wish you’ll shine in your life! Goodluck with all the barriers(/\\) in your life",c);
+
+return 0;
+
+}
---
+++ C-programming/module-2.5/p-5.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>=10000){
+        printf("gucci bag\n");
+        {
+            if(n>20000){
+                printf("gucci belt");
+            }
+        }
+    }
+    else if(n>=5000){
+        printf("levis Bag");
+
+    }
+    else{
+        printf("someting!");
+
+    }
+      return 0;
+}
+
+
+
