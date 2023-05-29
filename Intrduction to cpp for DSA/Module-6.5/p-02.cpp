e920102c2b7be0ef5b87fb4a5ca93d7c5b49f4a5
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-05-30 02:40:44 +0600

    end module-6.5

--- Intrduction to cpp for DSA/Module-6.5/p-02.cpp
+++ Intrduction to cpp for DSA/Module-6.5/p-02.cpp
@@ -9,23 +9,27 @@ int main()
 fast;
 string s;
 getline(cin,s);
-// stringstream ss(s);
-// string word;
-for(ll i=0;i<s.size();i++){
-   string s2;
-   while(s[i]!=' ' && i<s.size()){
-    s2.pb(s[i]);
-    i++;
-   }
-   reverse(s2.begin(),s2.end());
-   if(i<s.size())
-   cout<<s2<<" ";
-   else{
-      cout<<s2<<endl;
-   }
-   
+stringstream ss(s);
+string word;
+// for(ll i=0;i<s.size();i++){
+//    string s2;
+//    while(s[i]!=' ' && i<s.size()){
+//     s2.pb(s[i]);
+//     i++;
+//    }
+//    reverse(s2.begin(),s2.end());
+//    if(i<s.size())
+//    cout<<s2<<" ";
+//    else{
+//       cout<<s2<<endl;
+//    }
+string s2;
+while(ss>>word){
+   reverse(word.begin(),word.end());
+   s2+=word;
+   s2+=" ";
 }
-
-
+s2.pop_back();
+cout<<s2<<endl;
 get_out;
 }
