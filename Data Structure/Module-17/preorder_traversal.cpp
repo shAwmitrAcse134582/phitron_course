ef2d17935e11540b2981e4411739142358bcd5d7
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-07-09 02:25:31 +0600

    Module is complete

---
+++ Data Structure/Module-17/preorder_traversal.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define ll  long long int
+#define pb  push_back
+#define get_out return 0
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val){
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+void preorder(Node *root){
+    //base case
+    if(root==NULL){
+        return;
+    }
+         cout<<root->val<<" ";
+         preorder(root->left);
+         preorder(root->right);
+
+}
+int main()
+{
+fast;
+//assign value to the Node
+Node *root=new Node(10);
+Node *a=new Node(20);
+Node *b=new Node(30);
+Node *c=new Node(40);
+Node *d=new Node(50);
+Node *e=new Node(60);
+Node *f=new Node(70);
+Node *g=new Node(80);
+Node *h=new Node(90);
+Node *i=new Node(100);
+
+//create connection
+root->left=a;
+root->right=b;
+a->left=c;
+a->right=h;
+c->right=e;
+h->right=i;
+b->right=d;
+d->left=f;
+d->right=g;
+
+preorder(root);
+
+
+get_out;
+}
