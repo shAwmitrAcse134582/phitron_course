64f592ca379061951f8aa5a8fac355340aa18a1f
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-07-12 00:35:55 +0600

    module end

---
+++ Data Structure/Module-19/Node_level.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        bool isOriginal;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+        
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int nodeLevel(TreeNode<int>* root, int k)
+{
+     queue<pair<TreeNode<int> *, int>> q;
+     q.push({root, 1});
+     while (!q.empty()) {
+        pair<TreeNode<int> *, int> f = q.front();
+       TreeNode<int> *node = f.first;
+       int level = f.second;
+       q.pop();
+       if (node->val == k) {
+         return level;
+       }
+       if (node->left) {
+         q.push({node->left, level + 1});
+       }
+       if (node->right) {
+         q.push({node->right, level + 1});
+       }
+    }
+}
