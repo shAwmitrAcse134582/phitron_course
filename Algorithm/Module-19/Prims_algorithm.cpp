2212c8a5d38e80e1559d26516f7e91ef738d38e2
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-08-31 03:12:18 +0600

    Minimum spanning tree,prims algorithm,kruskal algorithm

---
+++ Algorithm/Module-19/Prims_algorithm.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#define ll  long long int
+#define pii pair<int,int>
+#define pb  push_back
+#define get_out return 0
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+const int N=1e5+7;
+vector<pii>v[N];
+bool vis[N];
+class Edge{
+    public:
+    int a,b,w;
+    Edge(int a,int b,int w){
+        this->a=a;
+        this->b=b;
+        this->w=w;
+    }
+
+};
+
+class cmp{
+    public:
+    bool operator()(Edge a,Edge b){
+        return a.w>b.w;
+    }
+};
+
+void prims(int s){
+    priority_queue<Edge,vector<Edge>,cmp>pq;
+    vector<Edge>edgeList;
+    pq.push(Edge(s,s,0));
+
+    while(!pq.empty()){
+        Edge parent = pq.top();
+        pq.pop();
+        int a=parent.a;
+        int b=parent.b;
+        int w=parent.w;
+        if(vis[b])continue;
+        vis[b]=true;
+        edgeList.push_back(parent);
+        for(int i=0;i<v[b].size();i++){
+            pii child=v[b][i];
+            if(!vis[child.first]){
+                pq.push(Edge(b,child.first,child.second));
+            }
+        }
+    }
+    edgeList.erase(edgeList.begin());
+    for(Edge val:edgeList){
+        cout<<val.a<<" "<<val.b<<" "<<val.w<<endl;
+    }
+
+}
+int main()
+{
+fast;
+int n,e;
+cin>>n>>e;
+while(e--){
+    int a,b,w;
+    cin>>a>>b>>w;
+    v[a].pb({b,w});
+    v[b].push_back({a,w});
+}
+prims(1);
+get_out;
+}
---
+++ Algorithm/Module-19/kruskal.cpp
@@ -0,0 +1,95 @@
+#include<bits/stdc++.h>
+#define ll  long long int
+#define pii pair<int,int>
+#define pb  push_back
+#define get_out return 0
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+const int N=1e5+7;
+class Edge
+{
+public:
+    int a, b, w;
+    Edge(int a, int b, int w)
+    {
+        this->a = a;
+        this->b = b;
+        this->w = w;
+    }
+};
+bool cmp(Edge a, Edge b)
+{
+    return a.w < b.w;
+}
+
+int parent[N];
+int parentSize[N];
+void dsu_set(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        parent[i] = -1;
+        parentSize[i] = 1;
+    }
+}
+int dsu_find(int node)
+{
+    while (parent[node] != -1)
+    {
+        node = parent[node];
+    }
+    return node;
+}
+void dsu_union(int a, int b)
+{
+    int leaderA = dsu_find(a);
+    int leaderB = dsu_find(b);
+    if (leaderA != leaderB)
+    {
+        if (parentSize[leaderA] > parentSize[leaderB])
+        {
+            // A leader
+            parent[leaderB] = leaderA;
+            parentSize[leaderA] += parentSize[leaderB];
+        }
+        else
+        {
+            parent[leaderA] = leaderB;
+            parentSize[leaderB] += parentSize[leaderA];
+        }
+    }
+}
+int main()
+{
+fast;
+ int n, e;
+    cin >> n >> e;
+    vector<Edge> v;
+    vector<Edge> ans;
+    dsu_set(n);
+    while (e--)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        v.push_back(Edge(a, b, w));
+    }
+    sort(v.begin(), v.end(), cmp);
+    for (Edge val : v)
+    {
+        int a = val.a;
+        int b = val.b;
+        int w = val.w;
+        int leaderA = dsu_find(a);
+        int leaderB = dsu_find(b);
+        if (leaderA == leaderB)
+            continue;
+        ans.push_back(val);
+        dsu_union(a, b);
+    }
+    for (Edge val : ans)
+    {
+        cout << val.a << " " << val.b << " " << val.w << endl;
+    }
+
+get_out;
+}
