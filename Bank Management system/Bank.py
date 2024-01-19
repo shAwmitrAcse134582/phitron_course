82199b8d24a7d4756d144002cf349f3c4574958e
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2024-01-19 14:28:22 +0600

    Bank Management system

---
+++ Bank Management system/Bank.py
@@ -0,0 +1,185 @@
+
+import random
+
+class Account:
+    def __init__(self) -> None:
+        self.accounts={}
+        self.total_balance=0
+        self.total_loan=0
+        self.loanStatus=1
+        self.isBankrupt=0
+    
+    def create_user(self):
+        name=input('Enter your name : ')
+        email=input('Enter your email : ')
+        password=input('Enter your password : ')
+        ac_type=input('Provide Account Type : (savings/Current) : ')
+        address=input('Enter your permanent address : ')
+        ac_number=random.randint(10,1000)
+
+        self.accounts[ac_number]={
+            'name':name,
+            'email':email,
+            'password':password,
+            'ac_type':ac_type,
+            'address':address,
+            'transactions':[],
+            'balance':0,
+            'loan':0
+        }
+        print(f'Congratutalion!!Successfully you have created Your account.Account Number : {ac_number} password : {password}')
+    
+    def withdraw(self,ac_number,amount):
+        if self.isBankrupt == 0:
+            if ac_number in self.accounts:
+                if self.accounts[ac_number]['balance']>=amount:
+                    self.total_balance-=amount
+                    self.accounts[ac_number]['balance']-=amount
+                    self.accounts[ac_number]['transactions'].append(amount)
+                    print(f'Congratulation!!You have successfully withdraw {amount} tk from your balance')
+                else:
+                    print('sorry!!You have no enough balance to withdraw')
+                
+            else:
+                print('sorry!!Account Number is not found.please provide valid account number!!')
+
+    def deposit(self,ac_number,amount):
+        if ac_number in self.accounts:
+            self.accounts[ac_number]['balance']+=amount
+            self.total_balance+=amount
+            self.accounts[ac_number]['transactions'].append(amount)
+            print(f'successfully you have deposited {amount} tk to your balance')
+        else:
+            print('sorry!!Account Number is not found.please provide valid account number!!')
+
+    def  available_balance(self,ac_number):
+        if ac_number in self.accounts:
+            print(f'Current Balance : {self.accounts[ac_number]["balance"]} tk')  
+        else:
+            print('sorry!!Account Number is not found.please provide valid account number!!')
+
+    def transfer_balance(self,sender,receiver,amount):
+        if sender in self.accounts and receiver in self.accounts:
+            if self.accounts[sender]['balance'] < amount:
+                print('You have no enough money in your account')
+
+            else:
+                self.accounts[sender]['balance']-=amount
+                self.accounts[sender]['transactions'].append(amount)
+                print(f'successfully transferred balance {amount} tk {sender} to {receiver}')
+                self.accounts[receiver]['balance'] +=amount
+                self.accounts[receiver]['transactions'].append(amount)
+                print(f'successfully receive balance {amount} tk {receiver} from {sender}')
+        else:
+            print('Sorry!! Account Number is not found. Please provide a valid account number!!')
+    
+    def check_transaction_history(self,ac_number):
+        if ac_number in self.accounts:
+            for transaction in self.accounts[ac_number]['transactions']:
+                print(f'Transaction History : {transaction}')
+        else:
+             print('sorry!!Account Number is not found.please provide valid account number!!')
+             
+    def takeLoan(self, ac_number, amount):
+        if self.loanStatus == True:
+            if ac_number in self.accounts:
+                if self.accounts[ac_number]['loan'] < 2:
+                    self.total_loan += amount
+                    self.accounts[ac_number]['loan'] += 1
+                    self.accounts[ac_number]['balance'] += amount
+                    self.total_balance -= amount
+                    self.accounts[ac_number]['transactions'].append(amount)
+                    print(f'You have taken a loan of {amount} tk successfully!!')
+                else:
+                    print('Sorry! You have already taken a loan two times')
+            else:
+                print('Sorry!! Account Number is not found. Please provide a valid account number!!')
+        else:
+            print('Loan is not available now!!')
+    
+    def transfer_balance(self,sender,receiver,amount):
+        if sender in self.accounts and receiver in self.accounts:
+            if self.accounts[sender]['balance'] < amount:
+                print('You have no enough money in your account')
+
+            else:
+                self.accounts[sender]['balance']-=amount
+                self.accounts[sender]['transactions'].append(amount)
+                print(f'successfully transferred balance {amount} tk {sender} to {receiver}')
+                self.accounts[receiver]['balance'] +=amount
+                self.accounts[receiver]['transactions'].append(amount)
+                print(f'successfully receive balance {amount} tk {receiver} from {sender}')
+        else:
+            print('Sorry!! Account Number is not found. Please provide a valid account number!!')
+    
+    def checkBankrupt(self,bankrupt):
+        self.bankrupt=bankrupt
+    
+    def delete_account(self,ac_number):
+        if ac_number in self.accounts:
+            del self.accounts[ac_number]
+            print(f'Account {ac_number} is deleted successfully!!')
+        else:
+            print('Sorry!! Account Number is not found. Please provide a valid account number!!')
+    def show_user(self):
+        if self.accounts:
+            for ac_number, val in self.accounts.items():
+                print(f"Account Number: {ac_number},Name:{val['name']},Email : {val['email']}, Address : {val['address']},Balance : {val['balance']}")
+        else:
+            print('Sorry!! Account Number is not found. Please provide a valid account number!!')
+
+    def check_bank_loan(self):
+        print(f'Total Bank loan is {self.total_loan}')
+    
+    def check_current_balance(self):
+        print(f'current Balance of the Bank : {self.total_balance}')
+    
+    def loanControl(self,status):
+        self.status=status
+    def isValidAccount(self,ac_number,password):
+        if ac_number in self.accounts and password == self.accounts[ac_number]['password']:
+            return True
+        else:
+            return False
+    
+    
+            
+        
+
+
+    
+
+    
+
+    
+
+
+
+
+
+    
+
+
+          
+               
+                   
+                
+
+               
+        
+
+
+                
+                   
+
+
+        
+
+
+    
+            
+
+                    
+
+
+    
---
+++ Bank Management system/main.py
@@ -0,0 +1,105 @@
+from Bank import Account
+def main():
+    bank=Account()
+    Admin={
+        'id' :1,
+        'Name':'admin',
+        'password':'123'
+    }
+    while(True):
+        print('*************************')
+        op=input('choose a option  for log in admin or user: (a/u): ')
+        if op=='a':
+            id=int(input('Enter Admin Id : '))
+            password=input('Enter Admin Password : ')
+            if(id==Admin['id'] and password==Admin['password']):
+                while(True):
+                    print('****Congratulation!!You are logged in as a admin!****')
+                    print("1. Create User")
+                    print("2. Delete User")
+                    print("3. Show all User Accounts")
+                    print("4. Show Total Balance")
+                    print("5. Show Total Loan Amount")
+                    print("6. Toggle Loan Feature")
+                    print("7. Make Bank declare as Bankrupt")
+                    print("8. Logout ")
+
+
+                    ch=int(input('choose option: '))
+                    if(ch==1):
+                        bank.create_user()
+                    elif(ch==2):
+                        Account_Number=int(input('Enter Account Number : '))
+                        bank.delete_account(Account_Number)
+                    elif(ch==3):
+                        bank.show_user()
+                    elif(ch==4):
+                        bank.check_current_balance()
+                    elif(ch==5):
+                        bank.check_bank_loan()
+                    elif(ch==6):
+                        ok=int(input('Enter 0 for off loan system: '))
+                        bank.loanControl(ok)
+                    elif(ch==7):
+                        ok=int(input('Enter 0 for declare bank as Bankrupt : '))
+                        bank.checkBankrupt(ok)
+                    elif(ch==8):
+                        break
+            else:
+                print('Invalid account Number or password.please provide valid account number and password!')
+        
+        elif op=='u':
+            print('choose option for Register or logged in  (R/L) : ')
+            ch=input('Choose option : ')
+
+            if(ch=='R'):
+                print('Enter valid Information : ')
+                bank.create_user()
+            elif(ch=='L'):
+                Account_Number=int(input('Enter Account Number: '))
+                password=input('Enter password: ')
+                if(bank.isValidAccount(Account_Number,password)):
+                    while(True):
+                        print('***********Welcome Back*********')
+                        print('*******You are logged In********')
+                        print('1.Deposit.')
+                        print('2.Withdraw.')
+                        print('3.Check Balance.')
+                        print('4.Transfer Balance')
+                        print('5.Transaction History.')
+                        print('6.Take Loan.')
+                        print('7.LogOut')
+                        
+
+                        ch=int(input('Choose option: '))
+
+                        if(ch==1):
+                            Account_Number=int(input('Enter Account Number : '))
+                            amount=int(input('Amount : '))
+                            bank.deposit(Account_Number,amount)
+                        elif(ch==2):
+                            Account_Number=int(input('Enter Account Number : '))
+                            amount=int(input('Amount : '))
+                            bank.withdraw(Account_Number,amount)
+                        elif(ch==3):
+                            Account_Number=int(input('Enter Account Number : '))
+                            bank.available_balance(Account_Number)
+                        elif(ch==4):
+                             Account_Number1=int(input('Enter Account Number : '))
+                             Account_Number2=int(input('Enter Account Number : '))
+                             amount=int(input('Amount : '))
+                             bank.transfer_balance(Account_Number1,Account_Number2,amount)
+                        elif(ch==5):
+                            Account_Number=int(input('Enter Account Number : '))
+                            bank.check_transaction_history(Account_Number)
+                        elif(ch==6):
+                            Account_Number=int(input('Enter Account Number : '))
+                            amount=int(input('Amount : '))
+                            bank.takeLoan(Account_Number,amount)
+                        elif(ch==7):
+                            break
+            else:
+                print('Invalid account Number or password.please provide valid account number and password!')
+
+if __name__=='__main__':
+    main()
