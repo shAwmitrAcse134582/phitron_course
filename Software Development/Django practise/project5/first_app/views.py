ef5ae96c9aa823a14be1c91d6a06e57ddd0b5350
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2024-08-06 21:36:55 +0600

    use of get and post

--- Software Development/Django practise/project5/first_app/views.py
+++ Software Development/Django practise/project5/first_app/views.py
@@ -2,23 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
 def about(request):
-    return render(request,'about.html')
-
-
-def form(request):
     sub_data = None
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
+        select = request.POST.get('select')
+
         sub_data = {
             'name': name,
             'email': email,
+            'select':select,
         }
-    return render(request, 'form.html', {'sub_data': sub_data})
+    return render(request, 'about.html', {'sub_data': sub_data})
 
-
-
-    
+def form(request):
+    return render(request, 'form.html')
