f259fe97b1c5827520dcd258e429337aafa2adb1
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2024-08-12 12:40:23 +0600

    implement widget

--- Software Development/Django practise/project5/first_app/form.py
+++ Software Development/Django practise/project5/first_app/form.py
@@ -1,11 +1,22 @@
 from django import forms
 
+
+#widget == field to html input
+
 class contactForm(forms.Form):
-    name = forms.CharField(label='username')
+    name = forms.CharField(label='username',initial='Shawmitra',help_text='Total length must be within 80 characters',required=False,widget=forms.Textarea(attrs={'id' : 'text_area','class' :'class1 class2'}))
     email = forms.EmailField(label='useremail')
-    age=forms.IntegerField()
-    weight=forms.FloatField()
-    balance=forms.DecimalField()
-    check=forms.BooleanField()
-    birthday=forms.DateField()
-    appoinment=forms.DateField()
+    # age = forms.IntegerField()
+    age=forms.CharField(widget=forms.NumberInput)
+    weight = forms.FloatField()
+    balance = forms.DecimalField(required=False,disabled=True)
+    check = forms.BooleanField()
+    birthday = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
+    appointment = forms.DateField(widget=forms.DateInput(attrs={'type' : 'datetime-local'}))
+    
+    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    
+    MEAL_CHOICES = [('p', 'Pepperoni'), ('m', 'Mushroom')]  
+    pizza = forms.MultipleChoiceField(choices=MEAL_CHOICES,widget=forms.CheckboxSelectMultiple) 
+    file =forms.FileField()
--- Software Development/Django practise/project5/first_app/views.py
+++ Software Development/Django practise/project5/first_app/views.py
@@ -23,14 +23,17 @@ def form_view(request):
     return render(request, 'form.html')
 
 def DjangoForm(request):
-    if request.method == 'POST':
-        form = contactForm(request.POST)
-        if form.is_valid():
-            # Process the form data here
-            return render(request, 'form_success.html', {'form': form.cleaned_data})
+        if request.method == 'POST':
+            form = contactForm(request.POST,request.FILES)
+            if form.is_valid():
+                file = form.cleaned_data['file']
+                with open('./first_app/upload/'+ file.name,'wb+') as destination:
+                     for chunk in file.chunks():
+                          destination.write(chunk)
+                     
+                print(form.cleaned_data)
+                return render(request, 'django_form.html', {'form': form})
         else:
-            print(form.errors)  # Print the errors if the form is invalid
-    else:
-        form = contactForm()
-    
-    return render(request, 'django_form.html', {'form': form})
+            form=contactForm()
+        
+        return render(request, 'django_form.html', {'form': form})
