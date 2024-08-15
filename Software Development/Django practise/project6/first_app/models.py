3c3f660507c2bd78e7eb423371e202ea5f721787
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2024-08-16 00:23:52 +0600

    model and modelform in django

---
+++ Software Development/Django practise/project6/first_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name =models.CharField(max_length=20)
+    roll =models.IntegerField(primary_key=True)
+    address=models.TextField()
+    mobile_No=models.IntegerField(default='012345678')
+
+
+    def __str__(self):
+       return f"Roll  : {self.roll} - {self.name}"
+    
