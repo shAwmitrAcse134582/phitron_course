97a8e35047903f38358a6539c378e8a7234f5eff
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2024-09-03 12:49:41 +0600

    login and registration part end

---
+++ Software Development/Django practise/blog_project_part_2/author/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render,redirect
+from . import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+
+
+
+# Create your views here.
+# def add_author(request):
+#     if request.method =='POST':
+#         author_form=forms.AuthorForm(request.POST)
+#         if author_form.is_valid():
+#             author_form.save()
+#             return redirect('add_author')
+#     else:
+#         author_form=forms.AuthorForm()
+#     return render(request,'add_author.html',{'form' : author_form})
+
+def register(request):
+    if request.method =='POST':
+        register_form=forms.RegestrationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request,'Account created succesfully')
+            return redirect('register')
+    else:
+        register_form=forms.RegestrationForm()
+    return render(request,'register.html',{'form' :register_form,'type':'Register'})
+def user_login(request):
+    if request.method =='POST':
+        form=AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            user_name=form.cleaned_data['username']
+            user_pass=form.cleaned_data['password']
+            user=authenticate(username=user_name,password=user_pass)
+            if user is not None:
+                messages.warning(request,'Logged in succesfully')
+                login(request,user)
+                return redirect('user_login')
+            else:
+                messages.warning(request,'Login Information is incorrect')
+                return redirect(register)
+    else:
+        form=AuthenticationForm()
+        return render(request,'register.html',{'form':form, 'type':'Login'})
+        
+
+
+    
