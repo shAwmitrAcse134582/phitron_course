4b71bfd1be9d6b9f52f6b0ca3b6cc7425f19d7f6
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2024-11-25 10:48:01 +0600

    done

---
+++ Software Development/Django practise/Auth/firstApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.user_login,name='login'),
+    path('logout/',views.user_logout,name='logout'),
+    path('profile/',views.profile,name='profile'),
+    path('passchange/',views.pass_change,name='passchange'),
+]
