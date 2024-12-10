d449fd156e04d987bee9c27e18e94c688b84411d
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2024-12-10 12:07:25 +0600

    user information save in database

--- Software Development/Django practise/Bank/accounts/forms.py
+++ Software Development/Django practise/Bank/accounts/forms.py
@@ -26,7 +26,7 @@ class UserRegistrationForm(UserCreationForm):
             gender=self.cleaned_data.get('gender')
             postal_code=self.cleaned_data.get('postal_code')
             country=self.cleaned_data.get('country')
-            birth_date=self.cleaned_data.get('city')
+            birth_date=self.cleaned_data.get('birth_date')
             city = self.cleaned_data.get('city')
             street_address = self.cleaned_data.get('street_address')
 
@@ -45,5 +45,20 @@ class UserRegistrationForm(UserCreationForm):
                 account_no = 100000+ our_user.id
             )
         return our_user
+    
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                 'class' : (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                ) 
+                
+            })
 
 
--- Software Development/Django practise/Bank/accounts/views.py
+++ Software Development/Django practise/Bank/accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import FormView
 from django.contrib.auth import login
+from .forms import UserRegistrationForm 
+from django.urls import reverse_lazy
 
 class UserRegistrationView(FormView):
-    template_name=''
-    form_class=''
-    success_url=''
+    template_name='accounts/user_registration.html'
+    form_class=UserRegistrationForm 
+    success_url=reverse_lazy('register')
 
 
     def form_valid(self, form):
