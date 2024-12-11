0b4a0b3cd38fc876a38bc641e7d696894143121a
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2024-12-11 22:48:10 +0600

    added transactions form

---
+++ Software Development/Django practise/Bank/transactions/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Transaction
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = [
+            'amount',
+            'transaction_type'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        self.user_account = kwargs.pop('account') # account value ke pop kore anlam
+        super().__init__(*args, **kwargs)
+        self.fields['transaction_type'].disabled = True # ei field disable thakbe
+        self.fields['transaction_type'].widget = forms.HiddenInput() # user er theke hide kora thakbe
+
+    def save(self, commit=True):
+        self.instance.account = self.user_account
+        self.instance.balance_after_transaction = self.account.balance
+        return super().save()
