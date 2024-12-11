afdb5394229d0644c9e727a658c4872a29eb8bfd
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2024-12-11 14:45:54 +0600

    profile updated

--- Software Development/Django practise/Bank/Bank/urls.py
+++ Software Development/Django practise/Bank/Bank/urls.py
@@ -3,10 +3,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponseRedirect
+from core.views import HomeView
 urlpatterns = [
+     path('', HomeView.as_view(),name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', lambda request: HttpResponseRedirect('/accounts/register/')),
+    # path('', lambda request: HttpResponseRedirect('/accounts/register/')),
 ]
 
 # accounts/login
--- Software Development/Django practise/Bank/accounts/views.py
+++ Software Development/Django practise/Bank/accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import FormView
-from django.contrib.auth import login
-from .forms import UserRegistrationForm 
+from django.contrib.auth import login,logout
+from .forms import UserRegistrationForm,UserUpdateForm
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
+from django.views import View
 
 class UserRegistrationView(FormView):
     template_name='accounts/user_registration.html'
@@ -14,3 +17,29 @@ class UserRegistrationView(FormView):
         user=form.save()
         login(self.request,user)
         return super().form_valid(form)
+    
+class UserLoginView(LoginView):
+    template_name = 'accounts/user_login.html'
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+class UserLogoutView(LogoutView):
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return reverse_lazy('home')
+    
+class UserBankAccountUpdateView(View):
+    template_name = 'accounts/profile.html'
+
+    def get(self, request):
+        form = UserUpdateForm(instance=request.user)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')  # Redirect to the user's profile page
+        return render(request, self.template_name, {'form': form})
