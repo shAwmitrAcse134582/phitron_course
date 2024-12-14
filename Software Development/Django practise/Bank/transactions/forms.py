631799d3a07c2c1dae595f67cb0a135f62c9d12d
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2024-12-15 01:26:04 +0600

    deposit,withdraw and loan form

--- Software Development/Django practise/Bank/transactions/forms.py
+++ Software Development/Django practise/Bank/transactions/forms.py
@@ -18,3 +18,46 @@ class TransactionForm(forms.ModelForm):
         self.instance.account = self.user_account
         self.instance.balance_after_transaction = self.account.balance
         return super().save()
+    
+
+class DepositForm(TransactionForm):
+    def clean_amount(self):
+        min_deposit_amount=100
+        amount=self.cleaned_data.get('amount')
+        if amount<min_deposit_amount:
+            raise forms.ValidationError(
+                f'You need to deposit at least {min_deposit_amount} tk'
+            )
+        return amount
+    
+
+class WithdrawForm(TransactionForm):
+    def clean_amount(self):
+        account=self.account
+        min_withdraw_amount=500
+        max_withdraw_amount=20000
+        balance=account.balance
+        amount=self.cleaned_data.get('amount')
+        if amount < min_withdraw_amount:
+            raise forms.ValidationError(
+                f'You can withdraw at least {min_withdraw_amount} tk'
+            )
+        
+        if amount>max_withdraw_amount:
+            raise forms.ValidationError(
+                f'You can withdraw at most {max_withdraw_amount} tk'
+
+            )
+        
+        if amount > balance:
+            raise forms.ValidationError(
+                f'You have {balance} tk im your account'
+                'You can not withdraw more than your account balance'
+            )
+        
+        return amount
+    
+class LoanRequestform(TransactionForm):
+    def clean_amount(self):
+        amount=self.cleaned_data.get('amount')
+        return amount
