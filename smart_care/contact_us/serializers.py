3659945fc2ab50ada6e96980f44806a36b0e934f
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2025-01-14 13:44:47 +0600

    mada contactus Serializer

---
+++ smart_care/contact_us/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from . import models
+class ContactUsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.ContactUs
+        fields='__all__'
